5b0fb202ca42e7e36a713b712c19a379cf5635cc
LoppLys <90832773+LoppLys@users.noreply.github.com>
2021-10-13 14:57:59 +0200

    Created a customer test class

---
+++ kassasystem/src/test/java/inte/project/CustomerTest.java
@@ -0,0 +1,13 @@
+package inte.project;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class CustomerTest {
+    
+    @Test
+    void constructorSetsName(){
+        
+    } 
+
+}
