e0b78c27539538183dc97cf2586b826f4e58c221
Marah Zeibak <zeibakmarah@gmail.com>
2021-10-14 14:12:08 +0200

    worked with ProductType.java

--- src/main/java/inte/project/ProductType.java
+++ src/main/java/inte/project/ProductType.java
@@ -1,4 +1,24 @@
 package inte.project;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProductType {
+    private String type;
+    private Map<ProductType,Collection<Product>> collection = new HashMap<>();
+
+    public ProductType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    //add product
+
+    //delet product
+
+    //search product
 }
