b7de184e3e9e8bfe832b78a294efd3d748243e9a
Marah Zeibak <zeibakmarah@gmail.com>
2021-10-14 13:32:09 +0200

    worked with product class Product.java

--- src/main/java/inte/project/Product.java
+++ src/main/java/inte/project/Product.java
@@ -1,4 +1,29 @@
 package inte.project;
 
 public class Product {
+    private int Id;
+    private String name;
+    private double prise;
+
+    public Product (int Id, String name, double prise) {
+        this.Id = Id;
+        this.name = name;
+        this.prise = prise;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrise() {
+        return prise;
+    }
+    public void setPrise(double newPrise) {
+        prise = newPrise;
+    }
+
 }
--- src/test/java/inte/project/ProductTest.java
+++ src/test/java/inte/project/ProductTest.java
@@ -1,7 +1,14 @@
 package inte.project;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class ProductTest {
+
+    @Test
+    void constructorTestId() {
+        Product product = new Product(207813,"screen",399.9);
+        assertEquals(207813,product.getId());
+    }
 
 }
