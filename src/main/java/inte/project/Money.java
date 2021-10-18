ec499ff890099e29a14a84a8d6a28423b78a1d54
KKarolinaa <karolina.klimek28@gmail.com>
2021-10-18 17:39:55 +0200

    Money klass och testfall för den. den borde vara komplett och redo att användas.

--- src/main/java/inte/project/Money.java
+++ src/main/java/inte/project/Money.java
@@ -1,16 +1,50 @@
 package inte.project;
 
 // ser till att all hantering av pengar hanteras konsekvent i hela systemet.
+// Hanterar endast SEK
 public class Money implements Comparable<Money> {
 
     private static final int ORE_PER_CROWN = 100;
-    private long amount;
+    private int amountInOre;
 
-    public Money(long amount){
-        this.amount = amount;
+    public Money(int amountInOre){
+        this.amountInOre = amountInOre;
+    }
+
+    public Money(int crown, int ore){
+        this.amountInOre = crown*ORE_PER_CROWN + ore;
+    }
+    private int getAmountOfCrown(){
+        return amountInOre /ORE_PER_CROWN;
+    }
+    private int getAmountOfOre(){
+        return amountInOre %ORE_PER_CROWN;
+    }
+
+    public Money add(int ore){
+        return (new Money(amountInOre + ore));
+    }
+    //Tar ett Money object och anropar första add metoden med dess värde.
+    public Money add(Money m){
+        return add(m.amountInOre);
+    }
+    public Money subtract(int ore){
+        return new Money (amountInOre - ore);
+    }
+    //tar ett money object och anropar första subtract metoden med dess värde.
+    public Money subtract(Money m){
+        return subtract(m.amountInOre);
+    }
+
+    public int getAmountInOre(){
+        return amountInOre;
     }
 
     public int compareTo(Money other){
-        return 0;
+        return amountInOre - other.amountInOre;
+    }
+
+    public String toString(){
+        return String.format("%s:%02d kr", getAmountOfCrown(), getAmountOfOre());
     }
 }
--- src/test/java/inte/project/MoneyTest.java
+++ src/test/java/inte/project/MoneyTest.java
@@ -1,11 +1,64 @@
 package inte.project;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
 
+    @Test
     void toStringPresentsAmount(){
-        Money first = new Money(23);
-        assertEquals(23, 23);
+        Money first = new Money(2300);
+        assertEquals("23:00 kr", first.toString());
+    }
+
+    @Test
+    void toStringWithOre(){
+        Money first = new Money(23,15);
+        assertEquals("23:15 kr", first.toString());
+    }
+
+    @Test
+    void fiveIsLessThanSix(){
+        Money five = new Money(500);
+        Money six = new Money(6,0);
+        assertEquals(-100, five.compareTo(six));
+    }
+    @Test
+    void fiveIsMoreThanThreeFifty(){
+        Money five = new Money(500);
+        Money threeFifty = new Money(3,50);
+        assertEquals(150, five.compareTo(threeFifty));
+    }
+
+    @Test
+    void fiveIsFive(){
+        Money five = new Money(500);
+        assertEquals(500, five.getAmountInOre());
+    }
+
+    @Test
+    void addFiveAndSix(){
+        Money five = new Money(500);
+        Money eleven = five.add(600);
+        assertEquals(1100, eleven.getAmountInOre());
+    }
+
+    @Test
+    void addTwoMoneyObjects(){
+        Money two = new Money(200);
+        Money twoFifty = new Money(2,50);
+        assertEquals("4:50 kr", two.add(twoFifty).toString());
+    }
+    @Test
+    void subtractFiveFromSix(){
+        Money six = new Money(600);
+        assertEquals("1:00 kr", six.subtract(500).toString());
+    }
+    @Test
+    void subtractTwoObjects(){
+        Money twoFifty = new Money(2,50);
+        Money two = new Money(200);
+        assertEquals("0:50 kr", twoFifty.subtract(two).toString());
     }
 
 }
