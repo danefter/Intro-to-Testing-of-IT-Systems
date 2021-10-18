f8c9229564542d3cb4533cdcbda7c1723b204e26
lurre001 <89309612+lurre001@users.noreply.github.com>
2021-10-18 16:27:41 +0200

    Added handler/database for customers

--- src/main/java/inte/project/CustomerHandler.java
+++ src/main/java/inte/project/CustomerHandler.java
@@ -1,5 +1,7 @@
 package inte.project;
 // Klass som innehåller datastrukturen med alla customers och kan hantera dessa.
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -19,28 +21,37 @@ public class CustomerHandler {
         customerHashMapAdress = new HashMap<>();
         customerHashMapPhoneNumber = new HashMap<>();
     }
-    public void addPrivatePerson(String name, String address, String email, String phoneNumber){
-        Customer customer = new PrivatePerson(name, address, email, phoneNumber);
+    // Adderar privateperson so that they may be found by name, adress, memberID and phone if the person doesn't already exist in the database
+    // if not member not added to membershipID hashmap
+    public void addPrivatePerson(String name, String address, String email, String phoneNumber, int birthYear) throws IllegalArgumentException{
+        Customer customer = new PrivatePerson(name, address, email, phoneNumber, birthYear);
         if(!customerHashSet.contains(customer)){
             customerHashSet.add(customer);
             customerHashMapName.put(customer.getName(), customer);
             customerHashMapAdress.put(customer.getAddress(), customer);
-            customerHashMapMembershipID.put(customer.getMembership().getMemberID(), customer);
+            if(customer.isMember()){
+                customerHashMapMembershipID.put(customer.getMembership().getMemberID(), customer);
+            }
             customerHashMapPhoneNumber.put(customer.getPhoneNumber(), customer);
+        }else{
+            throw new IllegalArgumentException("Customer already exists");
         }
 
     }
-    public Customer getCustomerByName(String name){
-        return customerHashMapName.get(name);
-    }
-    public Customer getCustomerByName(String adress){
-        return customerHashMapName.get(adress);
+    public Collection<Customer> getAllCustomers(){
+        return Collections.unmodifiableCollection(customerHashSet);
     }
     public Customer getCustomerByName(String name){
         return customerHashMapName.get(name);
     }
-    public Customer getCustomerByName(String name){
-        return customerHashMapName.get(name);
+    public Customer getCustomerByAdress(String adress){
+        return customerHashMapAdress.get(adress);
+    }
+    public Customer getCustomerByMembershipID(int mID){
+        return customerHashMapMembershipID.get(mID);
+    }
+    public Customer getCustomerByPhoneNumber(String phone){
+        return customerHashMapPhoneNumber.get(phone);
     }
 
 }
--- src/test/java/inte/project/CustomerHandlerTest.java
+++ src/test/java/inte/project/CustomerHandlerTest.java
@@ -6,11 +6,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerHandlerTest {
     @Test
+    void addNewCustomerExistHashSet(){
+        CustomerHandler ch = new CustomerHandler();
+        ch.addPrivatePerson("name", "address", "email", "0707990998", 1997);
+        assertFalse(ch.getAllCustomers().isEmpty());
+    }
+    @Test
     void addNewCustomerExistHashMap(){
-        Customer customer = new PrivatePerson("name", "address", "email", "0707990998", 1997);
-        customer.setMembership();
-        customer.removeMembership();
-        assertFalse(customer.isMember());
+        CustomerHandler ch = new CustomerHandler();
+        ch.addPrivatePerson("name", "address", "email", "0707990998", 1997);
+        assertNotNull(ch.getCustomerByName("name"));
+    }
+    @Test
+    void addNewCustomerExistHashMapAdress(){
+        CustomerHandler ch = new CustomerHandler();
+        ch.addPrivatePerson("name", "address", "email", "0707990998", 1997);
+        assertNotNull(ch.getCustomerByAdress("address"));
+    }
+
+    @Test
+    void addNewCustomerExistHashMapPhone(){
+        CustomerHandler ch = new CustomerHandler();
+        ch.addPrivatePerson("name", "address", "email", "0707990998", 1997);
+        assertNotNull(ch.getCustomerByPhoneNumber("0707990998"));
     }
 
 }
