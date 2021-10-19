2f3849857e183ad248610583e08aed1ad9c26a2c
lurre001 <89309612+lurre001@users.noreply.github.com>
2021-10-19 16:20:10 +0200

    Update CustomerHandlerTest.java

--- src/test/java/inte/project/CustomerHandlerTest.java
+++ src/test/java/inte/project/CustomerHandlerTest.java
@@ -55,13 +55,18 @@ class CustomerHandlerTest {
     @Test
     void largerCustomerHandlerTest(){
         CustomerHandler customerHandler = new CustomerHandler();
-        customerHandler.addCustomer(new PrivatePerson("Person person", "Adressvägen 4", "personperson@gmail.com", "0712345678", "1990-01-01");
+        customerHandler.addCustomer(new PrivatePerson("Person person", "Adressvägen 4", "personperson@gmail.com", "0712345678", "1990-01-01"));
         customerHandler.addCustomer(new PrivatePerson("lukas", "Adressvägen 5", "lukas@gmail.com", "1612345678", "1991-01-01"));
         customerHandler.addCustomer(new PrivatePerson("maggie", "Adressvägen 6", "maggie@gmail.com", "2512345678", "1992-01-01"));
         customerHandler.addCustomer(new PrivatePerson("magnus", "Adressvägen 7", "magnus@gmail.com", "3412345678", "1993-01-01"));
         customerHandler.addCustomer(new PrivatePerson("blim", "Adressvägen 8", "blim@gmail.com", "4312345678", "1994-01-01"));
         customerHandler.addCustomer(new PrivatePerson("blam", "Adressvägen 9", "blam@gmail.com", "5212345678", "1995-01-01"));
         customerHandler.removeCustomer(customerHandler.getCustomerByAdress("Adressvägen 4"));
+        customerHandler.removeCustomer(customerHandler.getCustomerByAdress("Adressvägen 5"));
+        customerHandler.removeCustomer(customerHandler.getCustomerByAdress("Adressvägen 6"));
+        customerHandler.removeCustomer(customerHandler.getCustomerByAdress("Adressvägen 7"));
+        customerHandler.removeCustomer(customerHandler.getCustomerByAdress("Adressvägen 8"));
+        customerHandler.removeCustomer(customerHandler.getCustomerByAdress("Adressvägen 9"));
         assertTrue(customerHandler.getAllCustomers().isEmpty());
     }
 }
