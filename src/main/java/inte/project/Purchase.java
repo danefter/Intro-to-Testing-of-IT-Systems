c72ce4facb0edfb131c9fbfa97bb6fc35991eeb9
danefter <90710978+danefter@users.noreply.github.com>
2021-10-19 10:40:34 +0200

    Create Purchase.java

---
+++ src/main/java/inte/project/Purchase.java
@@ -0,0 +1,61 @@
+package inte.project;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+public class Purchase {
+
+    private int currentTotal;
+    private int currentPayment;
+    private String dateOfPurchase;
+
+    private HashMap<String, Product> productsToPurchase = new HashMap<>();
+  
+    private HashMap<String, Payment> paymentMethods = new HashMap<>();
+
+    public void Purchase(Product... products) {
+        for (Product p: products) {
+            this.currentTotal += p.getPrice();
+            this.productsToPurchase.put(p.getId(), p);
+        }
+    }
+
+    public void payForProducts(int amount) {
+        do {
+            Payment payment = new Payment();
+            currentPayment += payment.getPayment(amount);
+            paymentMethods.put(payment.getPaymentType(), payment);
+        }
+        while (currentPayment < currentTotal);
+        setDateOfPurchase();
+    }
+
+
+    public void setDateOfPurchase() {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        Date today = new Date();
+        dateOfPurchase = formatter.format(today);
+    }
+
+    public int getCurrentTotal() {
+        return currentTotal;
+    }
+
+    public void setCurrentTotal(int currentTotal) {
+        this.currentTotal = currentTotal;
+    }
+
+    public int getCurrentPayment() {
+        return currentPayment;
+    }
+
+    public void setCurrentPayment(int currentPayment) {
+        this.currentPayment = currentPayment;
+    }
+
+    public String getDateOfPurchase() {
+        return dateOfPurchase;
+    }
+
+}
