f043c3e6dcce5e4678aa0d6cea0b5c17bfd6bc70
danefter <90710978+danefter@users.noreply.github.com>
2021-10-20 07:29:15 +0200

    Update PaymentTest.java

--- src/test/java/inte/project/PaymentTest.java
+++ src/test/java/inte/project/PaymentTest.java
@@ -9,11 +9,23 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PaymentTest {
+<<<<<<< Updated upstream
 
     public PaymentTest() {}
 
     @Test
     void cardPaymentConstructor() {}
+=======
+    public PaymentTest() {}
+
+    @Test
+    void cardPaymentConstructor() {
+        PrivatePerson person = new PrivatePerson("Albin Ahl", "Regngatan 33", "abbeAhl@gmail.com", "0707896779", "1993-6-5");
+        DebitCard card = new DebitCard("Debitcard", person, new Money(100000000));
+        Payment payment = new Payment(new Money(5000, 0), card);
+        Assertions.assertEquals("Card", payment.getPaymentType());
+    }
+>>>>>>> Stashed changes
 
     @Test
     void multipleCardPaymentConstructor() {
@@ -21,10 +33,24 @@ public class PaymentTest {
     }
 
     @Test
+<<<<<<< Updated upstream
     void cashPaymentConstructor() {}
 
     @Test
     void pointsPaymentConstructor() {}
+=======
+    void cashPaymentConstructor() {
+        PrivatePerson person = new PrivatePerson("Albin Ahl", "Regngatan 33", "abbeAhl@gmail.com", "0707896779", "1993-6-5");
+        DebitCard card = new DebitCard("Debitcard", person, new Money(100000000));
+        GiftCard card1 = new GiftCard("Giftcard", person, new Money(100000000));
+        Payment payment = new Payment(new Money(5000, 0), card, card1);
+    }
+
+    @Test
+    void pointsPaymentConstructor() {
+        Payment payment = new Payment(new Money(5000, 0));
+    }
+>>>>>>> Stashed changes
 
     @Test
     void collectCashPayments() {}
