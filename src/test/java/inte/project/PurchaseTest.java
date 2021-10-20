3f6b2cf7d3c6cc718880e4c171e7b25a6a98e51d
danefter <90710978+danefter@users.noreply.github.com>
2021-10-20 07:51:49 +0200

    Update PurchaseTest.java

--- src/test/java/inte/project/PurchaseTest.java
+++ src/test/java/inte/project/PurchaseTest.java
@@ -40,7 +40,6 @@ public class PurchaseTest {
         Product product2 = new Tele("341276", "Mobile", new Money(10000));
         Product product3 = new HouseHold("346576", "Mixer", new Money(1500));
         Purchase purchase = new Purchase(product, product1, product2, product3);
-<<<<<<< Updated upstream
         Cash cash = new Cash(20, 2000);
         Cash cash1 = new Cash(20, 5000);
         Payment payment = new Payment(purchase.getCurrentTotal(), cash, cash1);
@@ -52,29 +51,27 @@ public class PurchaseTest {
     void purchaseProductsWithPoints() {
         Product product = new Appliances("348723", "Fridge", new Money(5000));
         Product product1 = new Appliances("347654", "Stove", new Money(4500));
-        Product product2 = new Tele("341276", "Mobile", new Money(10000));
+        Product product2 = new Tele("341276", "Mobile", new Money(100000000));
         Product product3 = new HouseHold("346576", "Mixer", new Money(1500));
         Purchase purchase = new Purchase(product, product1, product2, product3);
-        Cash cash = new Cash(20, 2000);
-        Cash cash1 = new Cash(20, 5000);
-        Payment payment = new Payment(purchase.getCurrentTotal(), cash, cash1);
-=======
-        Cash cash = new Cash(20, 2000);
-        Cash cash1 = new Cash(20, 5000);
-        Payment payment = new Payment(purchase.getCurrentTotal(), cash, cash1);
+        MembershipPoints points = new MembershipPoints(purchase.getCurrentTotal().getAmountInOre());
+        Payment payment = new Payment(purchase.getCurrentTotal(), points);
         purchase.payTotalForProducts(payment);
+        Assertions.assertEquals(purchase.getCurrentTotal(), purchase.getCurrentPayment());
     }
 
     @Test
-    void purchaseProductsWithPoints() {
+    void purchaseProductsWithSeparateMethods() {
         Product product = new Appliances("348723", "Fridge", new Money(5000));
         Product product1 = new Appliances("347654", "Stove", new Money(4500));
         Product product2 = new Tele("341276", "Mobile", new Money(10000));
         Product product3 = new HouseHold("346576", "Mixer", new Money(1500));
         Purchase purchase = new Purchase(product, product1, product2, product3);
-        Payment payment = new Payment(purchase.getCurrentTotal());
->>>>>>> Stashed changes
-        purchase.payTotalForProducts(payment);
-        Assertions.assertEquals(purchase.getCurrentTotal(), payment.getPayment());
+        Cash cash = new Cash(200, 10);
+        MembershipPoints points = new MembershipPoints(5000);
+        Payment payment = new Payment(new Money(5000), points);
+        Payment payment2 = new Payment(new Money(2000), cash);
+        purchase.paySeparatelyForProducts(payment, payment2);
+        Assertions.assertEquals(purchase.getCurrentTotal(), purchase.getCurrentPayment());
     }
 }
