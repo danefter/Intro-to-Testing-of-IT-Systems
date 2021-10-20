b3f0d25408b5d7d9cb9d6a895d2c51d7b9db198e
lurre001 <89309612+lurre001@users.noreply.github.com>
2021-10-20 17:35:41 +0200

    Membership old functionality removed

--- src/main/java/inte/project/Membership.java
+++ src/main/java/inte/project/Membership.java
@@ -36,47 +36,7 @@ public class Membership {
     public MembershipPoints getMembershipPoints(){
         return membershipPoints;
     }
-
- /*   private void setMembershipID(Customer customer){ //ID is set via hashCode from orgNumber or just the yearOfBirth (depending on if customer is PrivatePerson or Company)
-        if(customer instanceof PrivatePerson){
-            PrivatePerson cu = (PrivatePerson)customer;
-            customer.getMembership().setMembershipID(cu.getYearOfBirth() * cu.getEmail().hashCode() * 2000);
-
-
-    public Membership getMembership(Customer customer){ 
-        return members.get(customer);
-    }
-
-    private void addMember(Customer customer){
-        if(!members.containsKey(customer)){
-            members.put(customer, this);
-        }      
-        customer.setIsMember(true);
-    }
-
-    public void removeMembership(Customer customer){ 
-        if(members.containsKey(customer)){
-            members.remove(customer);
-        }
-        customer.setIsMember(false);
-    }
-
-    public Customer getMember(int id){ 
-        Customer customerToBeReturned = null;
-        Iterator<Map.Entry<Customer, Membership>> iter = members.entrySet().iterator();
-        while(iter.hasNext()){
-            Map.Entry<Customer, Membership> entry = iter.next();
-            Customer customer = entry.getKey();
-            Membership membership = entry.getValue();
-            if(membership.getMemberID() == id){
-                customerToBeReturned = customer;
-            }
-        }
-        return customerToBeReturned;
-    }   
-
-
-*/
+    
 
     public String toString(){
         return "Membership\n Created on: " + membershipCreatedDate + membershipPoints.toString() + "\n";
